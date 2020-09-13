937b994fe34e2995812957764845a98a4d84051b
Rupali-B <32805196+Rupali-B@users.noreply.github.com>
2020-09-13 14:39:27 -0700

    Update findKeywords.py

--- findKeywords.py
+++ findKeywords.py
@@ -1,1 +1,10 @@
 #finding the most common keywords in the string
+
+from collections 
+import Counter
+#text=''
+words = text.split()
+print (words)
+Counter = Counter(words)
+top_twenty = Counter.most_common(20)
+print(top_twenty)
